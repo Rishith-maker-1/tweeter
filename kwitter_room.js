25b05c24db86e2452586f2c4cd9c07da5b7955fb
Rishith-maker-1 <83953765+Rishith-maker-1@users.noreply.github.com>
2021-10-24 17:33:00 +0530

    Update kwitter_room.js

--- kwitter_room.js
+++ kwitter_room.js
@@ -17,16 +17,24 @@ function addroom() {
     purpose : "adding room name"
   });
   localStorage.setItem("room_name",room_name);
+      window.location="kwitter_page.html";
 }
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
      Room_names = childKey;
     //Start code
-    
+    console.log("Room Name - " + Room_names);
+      row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)'> # " + Room_names + "</div><hr>";
+      document.getElementById("output").innerHTML+= row;
     //End code
     });});}
 getData();
 
 function logout() {
     window.location = "index.html";
+}
+function redirectToRoomName(name) {
+  console.log(name);
+  localStorage.setItem("room_name",name);
+  window.location="kwitter_page.html";     
 }
